91f8116306867f2fc793a923d7e3cc4048576da9
tsukimizake <shomasd@gmail.com>
2024-04-08 17:59:51 +0900

    updating to IVE pin style

--- app/IveRearAdapter.hs
+++ app/IveRearAdapter.hs
@@ -13,29 +13,33 @@ run = do
 catchRight :: Model3d
 catchRight =
   (box 40 6 20 & translate (5, 0, 0))
-    `mappend` ( linearExtrudeDefault 20 (polygon 3 [[(0, 0), (0, 50), (35, 50), (35, 25), (20, 0)]])
-                  & rotate3d (5, -90, 0)
-                  & translate (20, 0, 0)
+    `mappend` ( linearExtrudeDefault 20 (polygon 3 [[(0, 0), (0, 50), (46, 50), (46, 25), (20, 0)]])
+                  & rotate3d (0, -90, 0)
+                  & translate (16, 0, 0)
               )
     `mappend` (polygon 3 [[(0, 0), (0, 22), (15, 0)]] & linearExtrudeDefault 20)
-    `difference` (boltHoleM5 & rotate3d (90, 0, 0) & translate (28, 10, 10))
-    `difference` (polygon 3 [[(-10, -10), (0, 12), (3, -3)]] & linearExtrudeDefault 50 & translate (-1, 0, -20))
+    `difference` (boltHoleM5 & rotate3d (90, 0, 0) & translate (25.5, 10, 10))
+    `difference` (polygon 3 [[(-10, -1), (-5, 22), (3, -1)]] & linearExtrudeDefault 60 & translate (-1, 0, -10))
     `difference` (screwHole M10 40 True & rotate3d (0, 90, 0) & translate (0, 25.5, 25))
     `difference` catcher
+    `difference` pinHole
+
+-- `difference`
 
 catchLeft :: Model3d
 catchLeft =
   mirror
     (1, 0, 0)
     ( (box 40 6 20 & translate (5, -3, 0))
-        `mappend` ( linearExtrudeDefault 20 (polygon 3 [[(0, 0), (0, 50), (35, 50), (35, 25), (20, 0)]])
-                      & rotate3d (5, -90, 0)
-                      & translate (20, 0, 0)
+        `mappend` ( linearExtrudeDefault 20 (polygon 3 [[(0, 0), (0, 50), (46, 50), (46, 25), (20, 0)]])
+                      & rotate3d (0, -90, 0)
+                      & translate (16, 0, 0)
                   )
         `mappend` (polygon 3 [[(0, 0), (0, 22), (15, 0)]] & linearExtrudeDefault 20)
-        `difference` (boltHoleM5 & rotate3d (90, 0, 0) & translate (28, 10, 10))
+        `difference` (boltHoleM5 & rotate3d (90, 0, 0) & translate (25.5, 10, 10))
         `difference` catcher
-        `difference` (polygon 3 [[(-10, -10), (0, 12), (7, 0)]] & linearExtrudeDefault 50 & translate (-1, 0, -20))
+        `difference` pinHole
+        `difference` (polygon 3 [[(-10, -1), (-5, 22), (3, -1)]] & linearExtrudeDefault 90 & translate (-1, 0, -20))
     )
     `difference` (screwHole M10 40 True & rotate3d (0, 90, 0) & translate (0, 25.5, 25))
 
@@ -47,12 +51,28 @@ boltHoleM11 = cylinder (11.5 / 2) 20 def
 
 catcher :: Model3d
 catcher =
-  polygon 3 [[(0, 0), (0, 6), (-2, 6), (0, 19), (15, 19), (17, 6), (15, 6), (15, 0)]]
-    & linearExtrudeDefault 10
-    & rotate3d (5, -90, 180)
-    & translate (0, 51, 10)
+  polygon 3 [[(0, 0), (1, 19), (12, 19), (13, 0)]]
+    & linearExtrudeDefault 35
+    & rotate3d (0, 0, 180)
+    & translate (12, 51, 5)
+
+pinHole :: Model3d
+pinHole =
+  cylinder (7 / 2) 46 def & translate (5.5, 41, 0)
 
 casterSide :: Model3d
 casterSide =
-  polygon 3 [[(0, -3), (0, 6), (-1, 6), (0, 19), (3, 19), (3, 0), (12, 0), (12, 19), (15, 19), (16, 6), (15, 6), (15, -3)]]
-    & linearExtrudeDefault 10
+  ( polygon 3 [[(0, 0), (1, 18.5), (12, 18.5), (13, 0)]]
+      & linearExtrudeDefault 34.5
+      & rotate3d (0, 0, 180)
+  )
+    `difference` ( hull
+                     [ cylinder (7 / 2) 46 def & translate (0, 0, 0),
+                       cylinder (7 / 2) 46 def & translate (0, -2, 0)
+                     ]
+                     & translate (-6.5, -10, 0)
+                 )
+    `mappend` ( polygon 3 [[(0, 0), (20, 0), (20, -10), (0, -10)]]
+                  & linearExtrudeDefault 35
+                  & rotate3d (0, 0, 180)
+              )
