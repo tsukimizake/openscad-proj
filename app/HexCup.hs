8543602884e5dfa31eaaa1a9de24476ec4a048e4
tsukimizake <shomasd@gmail.com>
2024-04-11 23:06:59 +0900

    make hex cup

--- app/HexCup.hs
+++ app/HexCup.hs
@@ -6,7 +6,24 @@ import HoneyCombWall
 import OpenSCAD
 
 sample :: OpenSCADM Model3d
-sample = honeyCombWallHex 10 50 5
+sample = do
+  wall <-
+    honeyCombWall 10 (100, 30, 4)
+      & fmap
+        ( rotate3d (0, -90, 0)
+        )
+
+  floor <- honeyCombWallHex 10 30 4
+  pure $
+    union
+      [ wall & translate (4, 0, 0) & rotate3d (0, 0, 60),
+        wall & translate (8, 0, 0) & translate (-30, 15, 0),
+        wall & rotate3d (0, 0, -60),
+        wall & translate (-4, 0, 0) & translate (-30, 15, 0) & translate (60, 0, 0),
+        wall & translate (4, 0, 0) & rotate3d (0, 0, 240) & translate (0, 60, 0),
+        wall & rotate3d (0, 0, 120) & translate (0, 60, 0),
+        floor
+      ]
 
 run :: IO ()
 run = render sample & writeFile "product.scad"
