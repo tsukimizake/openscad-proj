8690189d3061d29ec16438973cb4525e9db67546
tsukimizake <shomasd@gmail.com>
2024-06-09 15:45:59 +0900

    less angle of guard

--- app/PaniaGuard.hs
+++ app/PaniaGuard.hs
@@ -14,14 +14,14 @@ obj =
       `difference` boltHole
 
 base :: OS.Model3d
-base = OS.box 18 6 13 & translate (-5, 0, -2)
+base = OS.box 18 5 13 & translate (-5, 0, -2)
 
 guardPlate :: OS.Model3d
 guardPlate =
   let triangle =
         [(0, 0), (10, 0), (70, 100), (0, 115), (0, 0)] & map (\(x, y) -> (x, y, 0))
    in pathExtrude triangle (cylinder 5 2 def)
-        & rotate3d (0, -90, -75)
+        & rotate3d (0, -90, -85)
         & translate (13, 0, 0)
 
 boltHole :: OS.Model3d
