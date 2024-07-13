2106ce6754e4b98f0cbe3f8efaf62b5f49b45616
tsukimizake <shomasd@gmail.com>
2024-07-14 00:10:52 +0900

    adding solveOnLine

--- src/SketchSolver.hs
+++ src/SketchSolver.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# OPTIONS_GHC -Wno-unrecognisepointd-pragmas #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 
@@ -12,6 +13,7 @@ import Control.Monad.Freer.Error
 import Control.Monad.Freer.Reader (Reader, ask, runReader)
 import Control.Monad.Freer.State
 import Data.Function ((&))
+import Data.Kind (Type)
 import Data.Maybe (mapMaybe)
 import qualified Data.Maybe as Maybe
 import OpenSCAD (Model2d, polygon)
@@ -59,12 +61,32 @@ repeatUntilFixpoint m = do
 
 solveOnLines :: SolverM ()
 solveOnLines = do
-  SolverState {onLines, sketch} <- readStat
-  mapM_ (solveOnLine sketch) onLines
+  SolverState {onLines} <- readStat
+  mapM_ (solveOnLine) onLines
 
-solveOnLine :: Sketch -> (Point, Line) -> SolverM ()
-solveOnLine sk (p, l) = do
-  undefined
+solveOnLine :: (Point, Line) -> SolverM ()
+solveOnLine (p, l) =
+  liftA2 (,) (getValue p) (getValue l) >>= \case
+    -- TODO check
+    -- TODO angle 0,90,180,270
+
+    -- 0 degree
+    ((Nothing, Just y), (Just lx, Just _ly, Just angle)) -> do
+      let x = lx + cos angle * y
+      putExact p.x x
+    ((Just x, Nothing), (Just _lx, Just ly, Just angle)) -> do
+      let y = ly - sin angle * x
+      putExact p.y y
+    ((Just x, Just y), (Nothing, Just _ly, Just angle)) -> do
+      let lx = x - cos angle * y
+      putExact l.x lx
+    ((Just x, Just y), (Just _lx, Nothing, Just angle)) -> do
+      let ly = y + sin angle * x
+      putExact l.y ly
+    ((Just x, Just y), (Just lx, Just ly, Nothing)) -> do
+      let angle = atan2 (y - ly) (x - lx)
+      putExact l.angle angle
+    _ -> pure ()
 
 generateModel :: SolverM Model2d
 generateModel = do
@@ -138,11 +160,31 @@ unifyIds l r = do
 -- helpers
 ----------
 
-getValue :: Id -> SolverM (Maybe Double)
-getValue id = do
-  SolverState {uf, exacts} <- readStat
-  case find id uf of
-    parent -> pure $ lookup parent exacts
+class HasValue a where
+  type Value a :: Type
+  getValue :: a -> SolverM (Value a)
+
+instance HasValue Id where
+  type Value Id = Maybe Double
+  getValue id = do
+    SolverState {uf, exacts} <- readStat
+    case find id uf of
+      parent -> pure $ lookup parent exacts
+
+instance HasValue Point where
+  type Value Point = (Maybe Double, Maybe Double)
+  getValue p = do
+    x <- getValue $ p.x
+    y <- getValue $ p.y
+    pure (x, y)
+
+instance HasValue Line where
+  type Value Line = (Maybe Double, Maybe Double, Maybe Double)
+  getValue l = do
+    x <- getValue $ l.x
+    y <- getValue $ l.y
+    angle <- getValue $ l.angle
+    pure (x, y, angle)
 
 assertJust :: Maybe a -> SolverM a
 assertJust = \case
@@ -165,6 +207,18 @@ updateUf :: Id -> Id -> SolverM ()
 updateUf l r = do
   SolverState {uf, eqs, exacts} <- readStat
   put $ (union l r uf, eqs, exacts)
+
+putExact :: Id -> Double -> SolverM ()
+putExact id v = do
+  stat <- readStat
+  let exacts = [(id, v)] ++ stat.exacts
+  put (stat.uf, stat.eqs, exacts)
+
+putEq :: Id -> Id -> SolverM ()
+putEq id1 id2 = do
+  stat <- readStat
+  let eqs = [(id1, id2)] ++ stat.eqs
+  put (stat.uf, eqs, stat.exacts)
 
 ----------
 -- error functions
