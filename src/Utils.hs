e2effc8bbb80600939ea3add1c0ee96e5f39ef42
tsukimizake <shomasd@gmail.com>
2024-09-22 17:35:52 +0900

    use openscad-hs-sketcher

--- src/Utils.hs
+++
@@ -1,9 +0,0 @@
-module Utils (safeHead, safeTail) where
-
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing
-safeHead (x : _) = Just x
-
-safeTail :: [a] -> [a]
-safeTail [] = []
-safeTail (_ : xs) = xs
