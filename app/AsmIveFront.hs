c1064e4741a5e842046c0531ab0545d74172c17d
tsukimizake <shomasd@gmail.com>
2024-10-07 19:02:35 +0900

    update asm

--- app/AsmIveFront.hs
+++ app/AsmIveFront.hs
@@ -1,6 +1,8 @@
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
 {-# HLINT ignore "Use newtype instead of data" #-}
+{-# HLINT ignore "Functor law" #-}
+{-# HLINT ignore "Redundant <&>" #-}
 module AsmIveFront (obj, run) where
 
 import qualified Data.Colour as Color
@@ -19,22 +21,32 @@ obj = do
   adapter <-
     IveFrontAdapter2.obj
       <&> withOrigin (50, 0, 0) (rotate3d (0, 190, 0))
-      <&> translate (42.1, -13.5, 46.8)
+      <&> translate (42.075, -13.5, 46.8)
       >>= declModule
 
   caster <-
     IveFrontCaster2.obj
       >>= declModule
   let inter = intersection [adapter, caster]
+  let ivePin' = ivePin & translucent 0.1 OpenSCAD.yellow & rotate3d (0, 90, 0) & translate (65, 24, 15)
+  let pinInter = intersection [union [adapter, caster], ivePin']
   pure $
     union
-      [ inter & translucent 0.5,
-        adapter & translucent 0.3,
-        caster & translucent 0.3
+      [ inter & translucent 0.8 OpenSCAD.red,
+        pinInter & translucent 0.8 OpenSCAD.red,
+        adapter & translucent 0.1 OpenSCAD.blue,
+        caster & translucent 0.1 OpenSCAD.green,
+        ivePin'
       ]
 
-translucent :: Double -> Model3d -> Model3d
-translucent opacity model = model & transparent (Color.withOpacity OpenSCAD.red opacity)
+translucent :: Double -> Color.Colour Double -> Model3d -> Model3d
+translucent opacity colour model = model & transparent (Color.withOpacity colour opacity)
+
+ivePin :: Model3d
+ivePin =
+  union
+    [ cylinder 53 (6.3 / 2) def
+    ]
 
 run :: IO ()
 run =
